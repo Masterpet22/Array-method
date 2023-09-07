fabe86ab57b0de748f51cd35a863fb77a8c5a09d
Masterpet22 <eprenza@gmail.com>
2023-09-07 16:53:48 -0400

    se crearon los metodos de array mas usados

---
+++ app.js
@@ -0,0 +1,31 @@
+//map
+const love = [1,1,1,1]
+console.log(love.map(item => 2));
+
+//filter
+const num = [1,2,3,4]
+console.log(num.filter(item => item > 2));
+
+//push
+const letter = ['a','b','c']
+letter.push('d')
+console.log(letter);
+
+//unshift
+const num2 = [4,5,6]
+num2.unshift(3)
+console.log(num2);
+
+//pop
+const vocals = ['a','e','i','p']
+vocals.pop()
+console.log(vocals);
+
+//shift
+const consonant = ['a','b','c']
+consonant.shift()
+console.log(consonant);
+
+//join
+const num3 = [1,2,3]
+console.log(num3.join("-"));
